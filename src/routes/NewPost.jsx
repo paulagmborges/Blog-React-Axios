7f7145f1e40fb98361c7726c7988dbef59a57b1c
paula borges <paulabzambeli@gmail.com>
2025-01-04 21:02:33 -0300

    create file newPost.css

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,8 +1,30 @@
-
+import'./NewPost.css'
 
 function NewPost() {
   return (
-    <div>NewPost</div>
+    <div className="new Post">
+      <h2>Inserir novo Post:</h2>
+      <form action="">
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input 
+          type="text"
+          name="title"
+          id="tilte"
+          placeholder="Digite o título"
+           />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea name="body" 
+          id="body" 
+          placeholder="Digite o conteúdo">
+          </textarea>
+        </div>
+        <input type="submit" value="Criar Post" className="btn"/>
+      </form>
+
+    </div>
   )
 }
 
